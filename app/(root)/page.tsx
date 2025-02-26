fc5e825f6247b5ca449a97530507aeb833cbdfc7
andbudha <andreibartov@gmail.com>
2025-02-26 13:14:43 +0100

    header comp added/styled

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from '@/components/ui/button';
-
 const Homepage = () => {
   return (
     <div>
       <h3>Homepage</h3>
-      <Button className="pr">Button</Button>
     </div>
   );
 };

207ca1bdc7f7352cfc08ab31d4598abbdb0ae45a
andbudha <andreibartov@gmail.com>
2025-02-27 10:53:08 +0100

    loading page created&styled

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import loader from '@/assets/loader.gif';
+const LoadingPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-full">
+      <Image src={loader} alt="loading" width={100} height={100} />
+    </div>
+  );
+};
+
+export default LoadingPage;

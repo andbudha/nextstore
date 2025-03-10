fc08aa1b363b57c495e916497a89621aa28f3702
andbudha <andreibartov@gmail.com>
2025-03-10 10:55:47 +0100

    num-formatting func with decimals created

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,9 @@ export function cn(...inputs: ClassValue[]) {
 export function convertPrismaObjToJsObj<T>(data: T): T {
   return JSON.parse(JSON.stringify(data));
 }
+
+//format num with decimals places
+export function formatNumberWithDecimals(num: number) {
+  const [int, decimal] = num.toString().split('.');
+  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`;
+}

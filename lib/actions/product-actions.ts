e3f4a2c22f8ed30faa0f0908fc482efd216e8c9d
andbudha <andreibartov@gmail.com>
2025-03-10 20:10:45 +0100

    getting product by its slug action added

--- lib/actions/product-actions.ts
+++ lib/actions/product-actions.ts
@@ -11,3 +11,10 @@ export async function getLatestProducts() {
   });
   return convertPrismaObjToJsObj(data);
 }
+
+//getting product by its slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
+}
